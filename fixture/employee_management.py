ae538b75b3415f5fd674edf051f3e46a307c6a31
isukhova <ibardaeva81@gmail.com>
2024-04-12 01:20:20 -0700

    test merge

--- fixture/employee_management.py
+++ fixture/employee_management.py
@@ -5,6 +5,8 @@ from selenium.webdriver.remote.webdriver import WebDriver
 class EmployeeManagement:
     home_button = "a[data-automation-id='menu_home']"
     list_of_widgets_header_texts = ".widget-header span:last-child"
+    list_of_configurations_texts = "//a[@ng-if='!menu.children && !!menu.uiSref && maxLevels <= 0']"
+
 
     def __init__(self, step: StepHelper, wd: WebDriver):
         self.step = step
@@ -15,3 +17,6 @@ class EmployeeManagement:
 
     def get_widgets_headers(self):
         return self.step.get_elements_texts(self.list_of_widgets_header_texts)
+
+    def get_widgets_configurations(self):
+        return self.step.get_elements_texts(self.list_of_configurations_texts)
